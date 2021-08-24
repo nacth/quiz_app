002558782867bf2542197ccb7a070ff36b05e101
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-08-24 11:12:48 +0700

    update widget data (stateful)

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   var questionIndex = 0;
+
   void answerQuestion() {
-    questionIndex++;
+    setState(() {
+      questionIndex++;
+    });
     print(questionIndex);
   }
 
