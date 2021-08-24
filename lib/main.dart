7748c2e3a3741a8602653234025e3185995feb7a
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-08-24 14:52:18 +0700

    use ternary expression

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,8 @@ class _MyAppState extends State<MyApp> {
     print(_questionIndex);
     if (_questionIndex < questions.length) {
       print('We have more questions!');
+    } else {
+      print('No more questions!');
     }
   }
 
@@ -46,15 +48,19 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['question']),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]['question']),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
