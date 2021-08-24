cd1fdcbdbcd84d9a11f0798648b19d3f19b1791f
Abdurrasyid Muhasibi <abdurrasyid.muhasibi.20@gmail.com>
2021-08-24 14:03:14 +0700

    mapping list to widget

--- lib/main.dart
+++ lib/main.dart
@@ -39,17 +39,19 @@ class _MyAppState extends State<MyApp> {
     ];
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Quiz App'),
-          ),
-          body: Column(
-            children: [
-              Question(questions[_questionIndex]),
-              Answer(_answerQuestion),
-              Answer(_answerQuestion),
-              Answer(_answerQuestion),
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]['question']),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+          ],
+        ),
+      ),
     );
   }
 }
